5134e53fd9ae9c55e952534f5b723d94e5f7d7fd
Harry Bowron <harry@bolt.com>
2023-01-04 03:29:57 -0500

    switch between active tabs

--- scripts/background.js
+++ scripts/background.js
@@ -2,52 +2,86 @@ chrome.commands.onCommand.addListener(runCommand);
 
 async function runCommand(command) {
   if (command === "toggle") {
-    toggleTab();
+    toggle();
   }
 }
 
-async function toggleTab() {
-  getCurrentTab().then(tab => {
-    if (tab) {
-      console.log(`url: ${tab.url}`);
+async function toggle() {
+  getCurrentTab().then((tab) => {
+    console.log(`url: ${tab?.url}`);
 
-      const ghMatch = tab.url.match(/https:\/\/github.com\/(?<owner>[^\s]+)\/(?<repo>[^\s]+)\/pull\/(?<pr>[\d]+)/i);
-      console.log(`gh match: ${ghMatch}`)
-      if (ghMatch) {
-        openGraphiteTab(
-          ghMatch.groups.owner,
-          ghMatch.groups.repo,
-          ghMatch.groups.pr
-        );
-        return;
-      }
+    const ghMatch = tab?.url.match(
+      /https:\/\/github.com\/(?<owner>[^\s]+)\/(?<repo>[^\s]+)\/pull\/(?<pr>[\d]+)/i
+    );
+    console.log(`gh match: ${ghMatch}`);
+    if (ghMatch) {
+      openGraphiteTab(
+        ghMatch.groups.owner,
+        ghMatch.groups.repo,
+        ghMatch.groups.pr
+      );
+      return;
+    }
 
-      const gtMatch = tab.url.match(/https:\/\/app.graphite.dev\/github\/pr\/(?<owner>[^\s]+?)\/(?<repo>[^\s]+?)\/(?<pr>[\d]+)/i);
-      console.log(`gt match: ${gtMatch}`);
-      if (gtMatch) {
-        openGithubTab(
-          gtMatch.groups.owner,
-          gtMatch.groups.repo,
-          gtMatch.groups.pr
-        );
-      }
+    const gtMatch = tab?.url.match(
+      /https:\/\/app.graphite.dev\/github\/pr\/(?<owner>[^\s]+?)\/(?<repo>[^\s]+?)\/(?<pr>[\d]+)/i
+    );
+    console.log(`gt match: ${gtMatch}`);
+    if (gtMatch) {
+      openGithubTab(
+        gtMatch.groups.owner,
+        gtMatch.groups.repo,
+        gtMatch.groups.pr
+      );
     }
   });
 }
 
-async function openGraphiteTab( owner, repo, prNumber ) {
-  chrome.tabs.create({
-    url: `https://app.graphite.dev/github/pr/${owner}/${repo}/${prNumber}`,
-    active: true
-  });
+function openGraphiteTab(owner, repo, prNumber) {
+  const url = `https://app.graphite.dev/github/pr/${owner}/${repo}/${prNumber}`;
+  chrome.tabs
+    .query({
+      currentWindow: true,
+      url: `${url}*`,
+    })
+    .then((tabs) => {
+      for (const tab of tabs) {
+        if (tab?.url.startsWith(url)) {
+          chrome.tabs.update(tab.id, { active: true });
+          console.log(`tab ${tab.id} made active`);
+          return;
+        }
+      }
+      chrome.tabs.create({
+        url: url,
+        active: true,
+      });
+      console.log(`opened ${url}`);
+    });
 }
 
-// ?no-redirect=1 required to stop Graphite extension from redirecting
-async function openGithubTab(owner, repo, prNumber) {
-  chrome.tabs.create({
-    url: `https://github.com/${owner}/${repo}/pull/${prNumber}?no-redirect=1`,
-    active: true,
-  });
+function openGithubTab(owner, repo, prNumber) {
+  const url = `https://github.com/${owner}/${repo}/pull/${prNumber}`;
+  chrome.tabs
+    .query({
+      currentWindow: true,
+      url: `${url}*`,
+    })
+    .then((tabs) => {
+      for (const tab of tabs) {
+        if (tab?.url.startsWith(url)) {
+          chrome.tabs.update(tab.id, { active: true });
+          console.log(`tab ${tab.id} made active`);
+          return;
+        }
+      }
+      chrome.tabs.create({
+        // ?no-redirect=1 required to stop Graphite extension from redirecting
+        url: `${url}?no-redirect=1`,
+        active: true,
+      });
+      console.log(`opened ${url}`);
+    });
 }
 
 async function getCurrentTab() {
